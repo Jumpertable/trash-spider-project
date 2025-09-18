f26555a3b880f90a9a8662dc5920ec4ffb7b0642
Charlotte <6730613009@psu.ac.th>
2025-09-18 07:26:34 +0700

    Second commit

--- src/main.cpp
+++ src/main.cpp
@@ -24,26 +24,63 @@
 // }
 
 
-#define LED_PIN 2
+// #define LED_PIN 2
+// #define BUTTON_PIN 4
+
+// void setup() {
+//   pinMode(LED_PIN, OUTPUT);
+//   pinMode(BUTTON_PIN, INPUT);
+//   Serial.begin(115200);
+// }
+
+// void loop() {
+//   int State = digitalRead(BUTTON_PIN);
+
+//   if (State == HIGH) { 
+//     analogWrite(LED_PIN, HIGH);
+//     delay(250);
+//     analogWrite(LED_PIN, LOW);
+//     delay(250);
+//     Serial.println("Blinking");
+// } else {
+//     analogWrite(LED_PIN, LOW);
+//     Serial.println("OFF");
+//   }
+// }
+
+
+#define LED_BUILTIN 2  
 #define BUTTON_PIN 4
 
+int dutyCycle = 0; 
+
+
 void setup() {
-  pinMode(LED_PIN, OUTPUT);
   pinMode(BUTTON_PIN, INPUT);
-  Serial.begin(115200);
+  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(115200);      
 }
 
 void loop() {
-  int State = digitalRead(BUTTON_PIN);
+  int state = digitalRead(BUTTON_PIN);
+  Serial.print("State: ");
+  Serial.println(state);
 
-  if (State == HIGH) { 
-    digitalWrite(LED_PIN, HIGH);
-    delay(250);
-    digitalWrite(LED_PIN, LOW);
-    delay(250);
-    Serial.println("Blinking");
-} else {
-    digitalWrite(LED_PIN, LOW);
-    Serial.println("OFF");
+  if (state == HIGH) { 
+    // Fade up
+     for (; dutyCycle <= 255; dutyCycle++) {
+          analogWrite(LED_BUILTIN, dutyCycle); 
+                delay(30);  
+                
+                if (digitalRead(BUTTON_PIN) == LOW) break;
+    }
+  } else if (state == LOW) { 
+    // Fade down
+    for (;dutyCycle >= 0; dutyCycle--){ 
+            analogWrite(LED_BUILTIN, dutyCycle);   
+            delay(100);  
+
+                  if (digitalRead(BUTTON_PIN) == HIGH) break;
+    }
   }
 }
