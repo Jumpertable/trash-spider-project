8aa776d5bf9d73a5756d8366290ad89f2a1dfcab
Charlotte <6730613009@psu.ac.th>
2025-09-19 19:10:06 +0700

    Thank you tutor

--- src/main.cpp
+++ src/main.cpp
@@ -85,52 +85,45 @@
 //   }
 // }
 
+#include <Arduino.h>
+
 #define LED_PIN 2
 #define SW1_PIN 4
-#define SW2_PIN 16
+#define SW2_PIN 5
 
-const int PWM_freq = 1000;        // 1000 Hz
-const int PWM_resolution = 8;     // 8-bit: (0–255)
-const int ledChannel = 0;         // set the PWM channel
+const int PWM_freq = 1000;        // 1 kHz
+const int PWM_resolution = 8;     // 8-bit (0–255)
+const int ledChannel = 0;         // PWM channel
 
 int dutyCycle = 0;
 
 void setup() {
-  pinMode(SW1_PIN, INPUT); 
-  pinMode(SW2_PIN, INPUT); 
+  pinMode(SW1_PIN, INPUT);
+  pinMode(SW2_PIN, INPUT);  
   pinMode(LED_PIN, OUTPUT);
-
 
   ledcSetup(ledChannel, PWM_freq, PWM_resolution);
   ledcAttachPin(LED_PIN, ledChannel);
-
-  Serial.begin(115200);
 }
 
 void loop() {
   int SW1 = digitalRead(SW1_PIN);
   int SW2 = digitalRead(SW2_PIN);
-  Serial.print("SW1: ");
-  Serial.println(SW1);
-  
-  Serial.print("SW2: ");
-  Serial.println(SW2);
 
-  if (SW1 == HIGH) {  
-    Serial.println("SW1 pressed, fade upppp");
-    for (; dutyCycle <= 255; dutyCycle++) {
-      ledcWrite(ledChannel, dutyCycle);
-      delay(10);
-
+  if (SW1 == HIGH && dutyCycle < 255) {   // fade up
+    while(dutyCycle<255){
+    dutyCycle++;
+    ledcWrite(ledChannel, dutyCycle);
+    delay(10);
     }
-  } 
-  else if (SW2 == HIGH) {  
-    Serial.println("SW2 pressed, fade dooooown");
-    for (; dutyCycle >= 0; dutyCycle--) {
-      ledcWrite(ledChannel, dutyCycle);
-      delay(10);
+  }
 
-      if (digitalRead(SW1) == HIGH) break;
+  if (SW2 == HIGH && dutyCycle > 0) {  
+    while(dutyCycle>0){   // fade down
+    dutyCycle--;
+    ledcWrite(ledChannel, dutyCycle);
+    delay(10);
     }
   }
 }
+
