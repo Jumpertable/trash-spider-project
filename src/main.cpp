7d123efee5c2aa2cbd6e6190dc156519c5fc89f0
Charlotte <6730613009@psu.ac.th>
2025-09-22 19:07:41 +0700

    Experiment1

--- src/main.cpp
+++ src/main.cpp
@@ -85,45 +85,82 @@
 //   }
 // }
 
-#include <Arduino.h>
+// #include <Arduino.h>
 
-#define LED_PIN 2
-#define SW1_PIN 4
-#define SW2_PIN 5
+// #define LED_PIN 2
+// #define SW1_PIN 4
+// #define SW2_PIN 5
 
-const int PWM_freq = 1000;        // 1 kHz
-const int PWM_resolution = 8;     // 8-bit (0–255)
-const int ledChannel = 0;         // PWM channel
+// const int PWM_freq = 1000;        // 1 kHz
+// const int PWM_resolution = 8;     // 8-bit (0–255)
+// const int ledChannel = 0;         // PWM channel
 
-int dutyCycle = 0;
+// int dutyCycle = 0;
+
+// void setup() {
+//   pinMode(SW1_PIN, INPUT);
+//   pinMode(SW2_PIN, INPUT);  
+//   pinMode(LED_PIN, OUTPUT);
+
+//   ledcSetup(ledChannel, PWM_freq, PWM_resolution);
+//   ledcAttachPin(LED_PIN, ledChannel);
+// }
+
+// void loop() {
+//   int SW1 = digitalRead(SW1_PIN);
+//   int SW2 = digitalRead(SW2_PIN);
+
+//   if (SW1 == HIGH && dutyCycle < 255) {   // fade up
+//     while(dutyCycle<255){
+//     dutyCycle++;
+//     ledcWrite(ledChannel, dutyCycle);
+//     delay(1);
+//     }
+//   }
+
+//   if (SW2 == HIGH && dutyCycle > 0) {  
+//     while(dutyCycle>0){   // fade down
+//     dutyCycle--;
+//     ledcWrite(ledChannel, dutyCycle);
+//     delay(1);
+//     }
+//   }
+// }
+
+#define PWM 2
+int dutyCycle = 127;  // start at 50%
+
+const int PWM_freq = 1000;      // 1000 Hz
+const int PWM_resolution = 8;   // 8-bit (0–255)
+const int ledChannel = 0;       // PWM channel
+
+#define SW1 26  
+#define SW2 27 
 
 void setup() {
-  pinMode(SW1_PIN, INPUT);
-  pinMode(SW2_PIN, INPUT);  
-  pinMode(LED_PIN, OUTPUT);
+  pinMode(PWM, OUTPUT);
+  pinMode(SW1, INPUT);
+  pinMode(SW2, INPUT);
 
   ledcSetup(ledChannel, PWM_freq, PWM_resolution);
-  ledcAttachPin(LED_PIN, ledChannel);
+  ledcAttachPin(PWM, ledChannel);
+
+  ledcWrite(ledChannel, dutyCycle);
 }
 
-void loop() {
-  int SW1 = digitalRead(SW1_PIN);
-  int SW2 = digitalRead(SW2_PIN);
-
-  if (SW1 == HIGH && dutyCycle < 255) {   // fade up
-    while(dutyCycle<255){
-    dutyCycle++;
+void loop() { 
+  if (digitalRead(SW1) == HIGH) {
+    dutyCycle -= 10;
+    if (dutyCycle < 0) dutyCycle = 0;
     ledcWrite(ledChannel, dutyCycle);
-    delay(1);
-    }
+    delay(200);  
   }
 
-  if (SW2 == HIGH && dutyCycle > 0) {  
-    while(dutyCycle>0){   // fade down
-    dutyCycle--;
+ 
+  if (digitalRead(SW2) == HIGH) {
+    dutyCycle += 10;
+    if (dutyCycle > 255) dutyCycle = 255;
     ledcWrite(ledChannel, dutyCycle);
-    delay(1);
-    }
+    delay(200);  
   }
 }
-
